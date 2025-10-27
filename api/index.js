09ba1d7f0be11f7bc64c3c711aeb6a19ed1e97ce
Luiz Laikovski <luizlaikovski@gmail.com>
2025-10-27 09:09:50 -0300

    teste 6

--- api/index.js
+++ api/index.js
@@ -7,7 +7,11 @@ const app = express();
 const port = 8800;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use('/', acolitosRoutes);
 
