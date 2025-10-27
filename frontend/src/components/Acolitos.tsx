9d2b349c5bde7b5c4d04442772a3f3bf80877343
Luiz Laikovski <luizlaikovski@gmail.com>
2025-10-27 09:01:42 -0300

    teste 2

--- frontend/src/components/Acolitos.tsx
+++ frontend/src/components/Acolitos.tsx
@@ -22,7 +22,18 @@ const Acolitos = () => {
             <Form setAcolitos={setAcolito} />
             <div className="w-[80dvw] grid grid-cols-3 gap-8 mt-8">
                 {acolito.length > 0 ? (
-                    acolito.map((item) => <Card key={item.id} {...item} />)
+                    acolito.map((item) => <Card 
+                    key={item.id}
+                    nome={item.nome}
+                    id={item.id}
+                    sexo={item.sexo}
+                    idade={item.idade}
+                    telefone={item.telefone}
+                    tamTunica={item.tamTunica}
+                    comunidades={item.comunidades}
+                    missas={item.missas}
+                    cerimonialista={item.cerimonialista}
+                    comentario={item.comentario} />)
                 ) : (
                     <p className="text-gray-500 text-lg mt-4">
                         Nenhum acólito encontrado com os filtros selecionados.
