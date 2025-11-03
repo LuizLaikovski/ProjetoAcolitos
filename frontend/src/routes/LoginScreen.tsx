5924239494d1a8105300ff84d1216351603ae777
Luiz Laikovski <luizlaikovski@gmail.com>
2025-11-03 08:06:05 -0300

    cssss

--- frontend/src/routes/LoginScreen.tsx
+++ frontend/src/routes/LoginScreen.tsx
@@ -52,7 +52,7 @@ export default function LoginScreen() {
                     <button
                         type="button"
                         onClick={() => setShowPassword(!showPassword)}
-                        className="absolute right-[-30vw] top-1/4 transform-translate-y-1/2 text-gray-500 hover:text-gray-700"
+                        className="absolute right-5 top-1/4 transform-translate-y-1/2 text-gray-500 hover:text-gray-700 eyesPassword"
                         aria-label={showPassword ? "Ocultar senha" : "Mostrar senha"}
                     >
                         <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
